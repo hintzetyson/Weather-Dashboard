e90a21d8360d307502f5bc81f0feb511ab8dc294
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-05-14 13:38:59 -0600

    added weather for other days

--- assets/script.js
+++ assets/script.js
@@ -96,3 +96,171 @@ $.ajax({
     else {
         $(".iconCurrent").attr("src", "assets/rainbow-128.png")
     }
+
+    // 1/5 forcast 
+    // var unixDtResponse = (response.daily[0].dt)  date response in unix time 
+    var dt1 = new Date(response.daily[1].dt * 1000);
+    dt1 = moment(dt1).format("MM/DD/YYYY")
+    $(".date1").text(dt1)
+    var tempF1 = (response.daily[1].temp.day - 273.15) * 1.80 + 32;
+    $(".tempF1").text("Temperature (F): " + tempF1.toFixed(0));
+    var tempC1 = (response.daily[1].temp.day - 273.15);
+    $(".tempC1").text("Temperature (C): " + tempC1.toFixed(0));
+    var windMPH1 = (response.daily[1].wind_speed * 2.237)
+    $(".wind1").text("Wind Speed: " + windMPH1.toFixed(0) + " Mph");
+    $(".humidity1").text("Humidity: " + response.daily[1].humidity + "%");
+    $(".UVIndex1").text("UV Index: " + response.daily[1].uvi.toFixed(1))
+
+    var weatherMain1 = response.daily[1].weather[0].main;
+    
+    if (weatherMain1 === "Clear") {
+        $(".icon1").attr("src", "assets/sun-128.png")
+    }
+    else if (weatherMain1 === "Drizzle", "Clouds") {
+        $(".icon1").attr("src", "assets/cloudy-128.png")
+    }
+    else if (weatherMain1 === "Rain") {
+        $(".icon1").attr("src", "assets/rain-cloud-128.png")
+    }
+    else if (weatherMain1 === "Snow") {
+        $(".icon1").attr("src", "assets/snow-128.png")
+    }
+    else if (weatherMain1 === "Thunderstorm") {
+        $(".icon1").attr("src", "assets/flash-cloud-128.png")
+    }
+
+    //------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------
+    // 2/5 forcast 
+    var dt2 = new Date(response.daily[2].dt * 1000);
+    dt2 = moment(dt2).format("MM/DD/YYYY")
+    $(".date2").text(dt2)
+    var tempF2 = (response.daily[2].temp.day - 273.15) * 1.80 + 32;
+    $(".tempF2").text("Temperature (F): " + tempF2.toFixed(0));
+    var tempC2 = (response.daily[2].temp.day - 273.15);
+    $(".tempC2").text("Temperature (C): " + tempC2.toFixed(0));
+    var windMPH1 = (response.daily[2].wind_speed * 2.237)
+    $(".wind2").text("Wind Speed: " + windMPH1.toFixed(0) + " Mph");
+    $(".humidity2").text("Humidity: " + response.daily[2].humidity + "%");
+    $(".UVIndex2").text("UV Index: " + response.daily[2].uvi.toFixed(1))
+
+    var weatherMain2 = response.daily[2].weather[0].main;
+     
+    if (weatherMain2 === "Clear") {
+        $(".icon2").attr("src", "assets/sun-128.png")
+    }
+    else if (weatherMain2 === "Clouds") {
+        $(".icon2").attr("src", "assets/cloudy-128.png")
+    }
+    else if (weatherMain2 === "Rain") {
+        $(".icon2").attr("src", "assets/rain-cloud-128.png")
+    }
+    else if (weatherMain2 === "Snow") {
+        $(".icon2").attr("src", "assets/snow-128.png")
+    }
+    else if (weatherMain2 === "Thunderstorm") {
+        $(".icon2").attr("src", "assets/flash-cloud-128.png")
+    }
+
+    //------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------
+    // 3/5 forcast 
+    var dt3 = new Date(response.daily[3].dt * 1000);
+    dt3 = moment(dt3).format("MM/DD/YYYY")
+    $(".date3").text(dt3)
+    var tempF3 = (response.daily[3].temp.day - 273.15) * 1.80 + 32;
+    $(".tempF3").text("Temperature (F): " + tempF3.toFixed(0));
+    var tempC3 = (response.daily[3].temp.day - 273.15);
+    $(".tempC3").text("Temperature (C): " + tempC3.toFixed(0));
+    var windMPH3 = (response.daily[3].wind_speed * 2.237)
+    $(".wind3").text("Wind Speed: " + windMPH3.toFixed(1) + " Mph");
+    $(".humidity3").text("Humidity: " + response.daily[3].humidity + "%");
+    $(".UVIndex3").text("UV Index: " + response.daily[3].uvi.toFixed(1))
+
+    var weatherMain3 = response.daily[3].weather[0].main;
+    
+    if (weatherMain3 === "Clear") {
+        $(".icon3").attr("src", "assets/sun-128.png")
+    }
+    else if (weatherMain3 === "Clouds") {
+        $(".icon3").attr("src", "assets/cloudy-128.png")
+    }
+    else if (weatherMain3 === "Rain") {
+        $(".icon3").attr("src", "assets/rain-cloud-128.png")
+    }
+    else if (weatherMain3 === "Snow") {
+        $(".icon3").attr("src", "assets/snow-128.png")
+    }
+    else if (weatherMain3 === "Thunderstorm") {
+        $(".icon3").attr("src", "assets/flash-cloud-128.png")
+    }
+
+    //------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------
+    // 4/5 forcast 
+    var dt4 = new Date(response.daily[4].dt * 1000);
+    dt4 = moment(dt4).format("MM/DD/YYYY")
+    $(".date4").text(dt4)
+    var tempF4 = (response.daily[4].temp.day - 273.15) * 1.80 + 32;
+    $(".tempF4").text("Temperature (F): " + tempF4.toFixed(0));
+    var tempC4 = (response.daily[4].temp.day - 273.15);
+    $(".tempC4").text("Temperature (C): " + tempC4.toFixed(0));
+    var windMPH4 = (response.daily[4].wind_speed * 2.237)
+    $(".wind4").text("Wind Speed: " + windMPH4.toFixed(1) + " Mph");
+    $(".humidity4").text("Humidity: " + response.daily[4].humidity + "%");
+    $(".UVIndex4").text("UV Index: " + response.daily[4].uvi.toFixed(1))
+
+    var weatherMain4 = response.daily[4].weather[0].main;
+    
+    if (weatherMain4 === "Clear") {
+        $(".icon4").attr("src", "assets/sun-128.png")
+    }
+    else if (weatherMain4 === "Clouds") {
+        $(".icon4").attr("src", "assets/cloudy-128.png")
+    }
+    else if (weatherMain4 === "Rain") {
+        $(".icon4").attr("src", "assets/rain-cloud-128.png")
+    }
+    else if (weatherMain4 === "Snow") {
+        $(".icon4").attr("src", "assets/snow-128.png")
+    }
+    else if (weatherMain4 === "Thunderstorm") {
+        $(".icon4").attr("src", "assets/flash-cloud-128.png")
+    }
+
+    //------------------------------------------------------------------------------------------
+
+    //------------------------------------------------------------------------------------------
+    // 5/5 forcast 
+    var dt5 = new Date(response.daily[5].dt * 1000);
+    dt5 = moment(dt5).format("MM/DD/YYYY")
+    $(".date5").text(dt5)
+    var tempF5 = (response.daily[5].temp.day - 273.15) * 1.80 + 32;
+    $(".tempF5").text("Temperature (F): " + tempF5.toFixed(0));
+    var tempC5 = (response.daily[5].temp.day - 273.15);
+    $(".tempC5").text("Temperature (C): " + tempC5.toFixed(0));
+    var windMPH5 = (response.daily[4].wind_speed * 2.237)
+    $(".wind5").text("Wind Speed: " + windMPH5.toFixed(1) + " Mph");
+    $(".humidity5").text("Humidity: " + response.daily[5].humidity + "%");
+    $(".UVIndex5").text("UV Index: " + response.daily[5].uvi.toFixed(1))
+
+    var weatherMain5 = response.daily[5].weather[0].main;
+    
+    if (weatherMain5 === "Clear") {
+        $(".icon5").attr("src", "assets/sun-128.png")
+    }
+    else if (weatherMain5 === "Clouds") {
+        $(".icon5").attr("src", "assets/cloudy-128.png")
+    }
+    else if (weatherMain5 === "Rain") {
+        $(".icon5").attr("src", "assets/rain-cloud-128.png")
+    }
+    else if (weatherMain5 === "Snow") {
+        $(".icon5").attr("src", "assets/snow-128.png")
+    }
+    else if (weatherMain5 === "Thunderstorm") {
+        $(".icon5").attr("src", "assets/flash-cloud-128.png")
+    }

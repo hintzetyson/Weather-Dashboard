2af26c66e495aca46ada002c3cf640bc74e3696e
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-05-14 09:33:24 -0600

    added variables, temperature, and dates

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,21 @@ $.ajax({
     method: 'GET'
 }).then(function (response) {
     console.log(response)
+
+    //vars to hold the longitude and latitude to be used 
+    var lon = response.coord.lon
+    var lat = response.coord.lat
+    $('.city').html("<h1>" + "City: " + response.name + "</h1>");
+    //convert to MPH
+    var windMPH = (response.wind.speed * 2.237);
+
+    $(".wind").text("Wind Speed: " + windMPH.toFixed(1) + " Miles per Hour");
+    $(".humidity").text("Humidity: " + response.main.humidity + "%");
+    //var to store conversion from kelvin to fahrenheit 
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    //displays the current temp in fahrenheit
+    $(".tempF").text("Current Temperature (F): " + tempF.toFixed(0));
+    //var to store conversion from kelvin to celsius 
+    var tempC = (response.main.temp - 273.15);
+    $(".tempC").text("Current Temperature (C): " + tempC.toFixed(0));
+    $(".date").text("Date: " + day);
